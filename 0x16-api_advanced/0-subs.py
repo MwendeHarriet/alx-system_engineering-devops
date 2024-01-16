45b4ff5a67ee2e636ddc22c28574f33fbabde0df
MwendeHarriet <mwendeharriet@gmail.com>
2024-01-16 02:41:39 -0800

    retuns number of subs in subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,42 +1,17 @@
+#!/usr/bin/python3
+"""This script queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit. If an
+    invalid subreddit is given, the function should return 0."""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Function returns number of subscribers on a subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()
-
-        if response.status_code == 200:
-            results = response.json().get("data", {})
-            subscribers = results.get("subscribers", 0)
-            return subscribers
-        elif response.status_code == 404:
-            return 0
-        else:
-            print("Unexpected response: {}".format(response.status_code))
-            return 0
-    except requests.exceptions.HTTPError as errh:
-        print("HTTP Error:", errh)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    data = response.json()
+    if response.status_code != 200:
         return 0
-    except requests.exceptions.ConnectionError as errc:
-        print("Error Connecting:", errc)
-        return 0
-    except requests.exceptions.Timeout as errt:
-        print("Timeout Error:", errt)
-        return 0
-    except requests.exceptions.RequestException as err:
-        print("Something went wrong:", err)
-        return 0
-
-
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
     else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+        return data['data']['subscribers']
