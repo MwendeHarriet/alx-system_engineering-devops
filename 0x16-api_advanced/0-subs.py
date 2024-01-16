5f42c9d3fa13ed2b7f16b0311145f1e6b4cb97b4
MwendeHarriet <mwendeharriet@gmail.com>
2024-01-16 02:45:23 -0800

    0-subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""This script queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit. If an
-    invalid subreddit is given, the function should return 0."""
-
+"""This script queries the Reddit API and returns the number of subscribers."""
 import requests
 
 
